855f2685bcd16c8a490eb8e2f20ed073da3d43dd
mutaqinmi21c <hehetaqin@gmail.com>
2023-11-27 18:34:15 +0700

    first commit

---
+++ app/Http/Controllers/InboxeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\inboxe;
+use App\Http\Requests\StoreinboxeRequest;
+use App\Http\Requests\UpdateinboxeRequest;
+
+class InboxeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreinboxeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(inboxe $inboxe)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(inboxe $inboxe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateinboxeRequest $request, inboxe $inboxe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(inboxe $inboxe)
+    {
+        //
+    }
+}
